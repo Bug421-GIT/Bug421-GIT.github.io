c0b9dbbca82df97cd4adf8809ae5e647c29acd63
Bug421-GIT <576896275@qq.com>
2024-05-11 20:33:35 +0800

    整理web在网络传输中表现

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1714219132';
+const cacheName = 'chirpy-1715429825';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
