c8652311645820b764b296ce7f6801f8350a1626
Bug421-GIT <576896275@qq.com>
2024-05-20 17:21:20 +0800

    上传服务工作者线程笔记

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1715429825';
+const cacheName = 'chirpy-1716196699';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
